d46d492a8d22763b0522ae443ed4f9c317ed693c
pmartin <contact@elol.fr>
2016-08-19 19:00:00 +0200

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
 # pgproc-www
 Access PostgresSQL procedures through HTTP
+
+## Prerequisites:
+
+ - pgproc module (https://github.com/actimeo/pgproc)
+ - PostgreSQL server 9.1 or higher
+ - PHP 5.5 or higher
+
+## Install
+
+- Install necessary PHP modules
+```sh
+$ composer install
+```
+
+- copy `config.inc.php.sample` to `config.inc.php`
+- copy `config.sh.sample` to `config.sh`
+- edit these 2 files with the name and credentials of your database you want to access
+
+- serve the subdirectory backend/htdocs with your preferred web server
+- Apache2 configuration:
+```
+<VirtualHost *:80>
+  [...]
+  Alias /pg "/path/to/pgproc-www/htdocs"
+  <Directory "/path/to/pgproc-www/htdocs">
+      Options Indexes MultiViews FollowSymlinks
+      AllowOverride None
+      Order allow,deny
+      Allow from all
+      RewriteEngine on
+      RewriteCond %{REQUEST_FILENAME} !-d
+      RewriteCond %{REQUEST_FILENAME} !-f
+      RewriteRule ^(.*)$ /pg/index.php?o=$1 [L,QSA]
+  </Directory>
+</VirtualHost>
+```
---
+++ htdocs/index.php
@@ -0,0 +1,98 @@
+<?php
+require '../vendor/autoload.php';
+require_once '../config.inc.php';
+
+use \actimeo\pgproc\PgProcedures;
+use \actimeo\pgproc\PgProcException;
+use \actimeo\pgproc\PgProcFunctionNotAvailableException;
+
+$base = new PgProcedures($pg_host, $pg_user, $pg_pass, $pg_database);
+
+$cmd = $_GET['o'];
+
+$body = json_decode(file_get_contents('php://input'));
+
+if (strpos($cmd, '/')) {
+  list($schema, $function) = explode('/', $cmd, 2);
+} else {
+  $schema = 'public';
+  $function = $cmd;
+}
+
+$debug = true;
+
+$ret = $base->pgprocedures->search_arguments ($schema, $function);
+
+$all = false;
+if (count ($ret)) {
+  foreach ($ret as $r) {
+    $all = true;
+    foreach ($r['argnames'] as $argname) {
+      if (!property_exists ($body, $argname)) {
+	if ($debug) {
+	  echo "$argname not found\n" ;
+	  print_r ($body);
+	}
+	$all = false;
+	break;
+      }
+    }
+    if ($all) {
+      break;
+    }    
+  }
+}
+
+// Continue, $r contains argnames and argtypes
+$args = array ();
+if ($all) {
+  foreach ($r['argnames'] as $argname) {
+    $args[] = get_magic_quotes_gpc() ? stripslashes($body->$argname) : $body->$argname;
+  }
+}
+
+if (isset($body->_count) && $body->_count === true)
+  $args[] = $base->count();
+
+if (isset($body->_order)) {
+  if (is_object($body->_order))
+    $args[] = $base->order($body->_order->col, strtoupper($body->_order->order));
+  else
+    $args[] = $base->order($body->_order);
+}
+
+if (isset($body->_limit)) {
+  if (is_object($body->_limit))
+    $args[] = $base->limit($body->_limit->limit, $body->_limit->offset);
+  else
+    $args[] = $base->limit($body->_limit);
+}
+
+if (isset($body->_distinct)) {
+  $args[] = $base->distinct();
+}
+
+try {
+  $results = $base->$schema->__call ($function, $args);
+} catch (PgProcFunctionNotAvailableException $e) {
+  header($_SERVER["SERVER_PROTOCOL"]." 404 Not Found"); 
+  echo $e->getMessage();
+  exit;
+} catch (PgProcException $e) {
+  header($_SERVER["SERVER_PROTOCOL"]." 400 Bad Request"); 
+  echo $e->getMessage();
+  exit;
+} catch (Exception $e) {
+  header($_SERVER["SERVER_PROTOCOL"]." 400 Bad Request"); 
+  echo $e->getMessage();
+  exit;
+}
+
+header ('Content-Type: application/json ; charset=utf-8');
+header ('Cache-Control: no-cache , private');
+header ('Pragma: no-cache');
+if ($results !== null) 
+  echo json_encode ($results);
+else 
+  echo '[]';
+exit;
